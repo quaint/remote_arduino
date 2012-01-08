a20892208128775ec4563ecdcf7887b49b099c8c
quaint <twisnia@gmail.com>
2012-01-08 20:50:41 +0100

    device sort fixed

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,7 +1,7 @@
 class Device < ActiveRecord::Base
   
   belongs_to :board
-  default_scope :order => 'board_id'
+  default_scope :order => 'name'
   
   validates :name, :presence => true, :uniqueness => true
   validates_numericality_of :port, :greater_than => 0, :less_than_or_equal_to => Proc.new { |device| device.board.outputs }, :only_integer => true, :allow_nil => true
